dbe034ee65b548c3cebe246de64dc60d1d14b4c4
JCoulter85 <jimcoulter85@gmail.com>
2018-10-24 21:30:11 -0700

    Newer Dish

--- src/app/dishdetail/dishdetail.component.spec.ts
+++ src/app/dishdetail/dishdetail.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DishDetailComponent } from './dishdetail.component';
+import { DishDetailComponent } from './DishDetail.component';
 
 describe('DishdetailComponent', () => {
   let component: DishDetailComponent;
