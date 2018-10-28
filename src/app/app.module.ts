dc7b9dc7381512dfbb2900c76795f1da34babd2a
JCoulter85 <jimcoulter85@gmail.com>
2018-10-28 15:18:06 -0700

    It broke again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { DishDetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishDetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+
 
 
 @NgModule({
@@ -24,7 +28,11 @@ import { FooterComponent } from './footer/footer.component';
     DishDetailComponent,
     HeaderComponent,
     FooterComponent,
-    
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    AppRoutingModule,
+
   ],
   imports: [
     BrowserModule,
@@ -37,6 +45,7 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule,
     MatListModule,
   ],
+
   providers: [
     DishService
   ],
