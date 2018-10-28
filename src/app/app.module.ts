baa68a852715582053ac2e2004f20c534ead3546
JCoulter85 <jimcoulter85@gmail.com>
2018-10-28 14:21:20 -0700

    Week 2 Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -20,6 +22,8 @@ import { DishService } from './services/dish.service';
     AppComponent,
     MenuComponent,
     DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
     
   ],
   imports: [
