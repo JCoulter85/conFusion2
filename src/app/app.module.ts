6c41b2c567777f876d8ca20b158be7f8137468d7
JCoulter85 <jimcoulter85@gmail.com>
2018-10-30 22:40:42 -0700

    Single Page Application Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,24 +16,23 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { DishDetailComponent } from './dishdetail/dishdetail.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { Dish } from './shared/dish';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent,
+    DishdetailComponent,
     HeaderComponent,
     FooterComponent,
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    
-
   ],
   imports: [
     BrowserModule,
