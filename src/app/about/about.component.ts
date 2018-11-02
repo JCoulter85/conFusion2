25d43b14cc78ef711e3ca4b0054644b622998d00
JCoulter85 <jimcoulter85@gmail.com>
2018-11-01 20:51:56 -0700

    Working Website Working Links Variable Issues

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
+
 export class AboutComponent implements OnInit {
 
   constructor() { }
