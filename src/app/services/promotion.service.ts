3806fee52ed445c9e35a7d5887a6b75daac2ad3a
JCoulter85 <jimcoulter85@gmail.com>
2018-11-25 13:49:41 -0800

    Observable Update

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +12,14 @@ export class PromotionService {
   constructor() { }
 
   getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+    return of(PROMOTIONS).pipe(delay(2000)).toPromise();
   }
 
   getPromotion(id: string): Promise<Promotion> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]),
-      2000);
-    });
+    return of(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]).pipe(delay(200)).toPromise();
   }
 
   getFeaturedPromotion(): Promise<Promotion> {
-    return new Promise(resolve => {
-      // Simulate server latancy with 2 second delay
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]),
-      2000);
-    });
+    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
