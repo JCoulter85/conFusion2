5591709905ca145b318eabc6012143e31645e353
JCoulter85 <jimcoulter85@gmail.com>
2018-11-25 17:36:25 -0800

    dishdetail.component.ts update

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,8 +20,7 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     let id = this.route.snapshot.params['id'];
-    this.dish = this.dishService.getDish(id)
-     .subscribe(dish => this.dish = dish);
+    this.dish = this.dishService.getDish(id).then(dish => this.dish = dish);
   }
 
   goBack(): void {
