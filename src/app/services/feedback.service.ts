99c2b212a86e1886863c9cdab91513231bf049e5
JCoulter85 <jimcoulter85@gmail.com>
2019-03-03 19:55:21 -0800

    post update

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -19,8 +19,8 @@ export class FeedbackService {
   constructor(private http: HttpClient,
     private processHTTPMsgService: ProcessHTTPMsgService) { }
 
-  save(feedback: Feedback) {
-    this.HttpClient.post('http://localhost:3000/feedback', feedback, {
+  save(feedback: Feedback): Observable<FeedbackService> {
+    return this.HttpClient.post('http://localhost:3000/contactus', feedback, {
       headers: new HttpHeaders({
         'Content-Type': 'application/jason'
       })
