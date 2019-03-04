a466290eb21186a8c75ce8c1f25c8b3dedd4fad1
JCoulter85 <jimcoulter85@gmail.com>
2019-03-03 19:17:33 -0800

    push update

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -23,31 +23,5 @@ export class FeedbackService {
       .pipe(catchError(this.handleError('addfeedback', feedback)));
   }
 
-  getDish(id: number): Observable<Dish> {
-    return this.http.get<Dish>(baseURL + 'dishes/' + id)
-      .pipe(catchError(this.processHTTPMsgService.handleError));
-  }
-
-  getFeaturedDish(): Observable<Dish> {
-    return this.http.get<Dish[]>(baseURL + 'dishes?featured=true').pipe(map(dishes => dishes[0]))
-      .pipe(catchError(this.processHTTPMsgService.handleError));
-  }
-
-  getDishIds(): Observable<number[] | any> {
-    return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id)))
-      .pipe(catchError(error => error));
-  }
-
-  putDish(dish: Dish): Observable<Dish> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
-    };
-
-    return this.http.put<Dish>(baseURL + 'dishes/' + dish.id, dish, httpOptions)
-      .pipe(catchError(this.processHTTPMsgService.handleError));
-
-  }
 }
 
